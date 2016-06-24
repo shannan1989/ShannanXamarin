79950491ec5ae328564cfab90d6d11062e48b5e9
山南 <zhangpeichaook@163.com>
2016-06-24 15:00:08 +0800

    Update

--- CodingHard/CodingHardPage.xaml.cs
+++ CodingHard/CodingHardPage.xaml.cs
@@ -4,15 +4,9 @@ namespace CodingHard
 {
 	public partial class CodingHardPage : TabbedPage
 	{
-		int count = 0;
 		public CodingHardPage()
 		{
 			InitializeComponent();
-		}
-		private void Button_Clicked(object sender, System.EventArgs e)
-		{
-			count++;
-			(sender as Button).Text = string.Format("{0} click{1}!",count,count==1?"":"s");
 		}
 	}
 }
