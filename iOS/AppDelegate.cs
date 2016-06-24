fefb48401fe04423a0a2c30a7411292e1236cbda
山南 <zhangpeichaook@163.com>
2016-06-24 13:38:55 +0800

    Fix

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -1,22 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
+﻿
 using Foundation;
 using UIKit;
 
 namespace CodingHard.iOS
 {
 	[Register("AppDelegate")]
-	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	public class AppDelegate : Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
 	{
-		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+		public override bool FinishedLaunching(UIApplication uiApplication, NSDictionary launchOptions)
 		{
-			global::Xamarin.Forms.Forms.Init();
+			Xamarin.Forms.Forms.Init();
 
 			LoadApplication(new App());
 
-			return base.FinishedLaunching(app, options);
+			return base.FinishedLaunching(uiApplication, launchOptions);
 		}
 	}
 }
