f74e71dd14be738de11ae4bf0ee152df6a53534b
山南 <zhangpeichaook@163.com>
2016-06-27 16:14:07 +0800

    EffectsPage

---
+++ CodingHard/EffectsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace CodingHard
+{
+	public partial class EffectsPage : ContentPage
+	{
+		public EffectsPage()
+		{
+			InitializeComponent();
+
+			entry.Effects.Add(Effect.Resolve("Xamarin.BackgroundColorEffect"));
+		}
+	}
+}
+
