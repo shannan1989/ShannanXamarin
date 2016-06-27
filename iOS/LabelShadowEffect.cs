9b09be8d41db0884fd1c5ad9b2e185586a93f7ac
山南 <zhangpeichaook@163.com>
2016-06-27 16:06:57 +0800

    LabelShadowEffect

---
+++ iOS/LabelShadowEffect.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using CodingHard.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ResolutionGroupName("Shannan")]
+[assembly: ExportEffect(typeof(LabelShadowEffect), "LabelShadowEffect")]
+namespace CodingHard.iOS
+{
+	public class LabelShadowEffect : PlatformEffect
+	{
+		protected override void OnAttached()
+		{
+			try
+			{
+				Control.Layer.CornerRadius = 5;
+				Control.Layer.ShadowColor = Color.Blue.ToCGColor();
+				Control.Layer.ShadowOffset = new CoreGraphics.CGSize(5, 5);
+				Control.Layer.ShadowOpacity = 1.0f;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Cannot set property on attached control. Error: {0}", ex.Message);
+			}
+		}
+
+		protected override void OnDetached()
+		{
+
+		}
+	}
+}
+
