445444eb13cc0ac47f5105a951220c17ff07a66c
山南 <zhangpeichaook@163.com>
2016-06-27 09:45:19 +0800

    NumericValidationBehavior

---
+++ CodingHard/Behaviors/NumericValidationBehavior.cs
@@ -0,0 +1,45 @@
+﻿using Xamarin.Forms;
+namespace CodingHard
+{
+	public static class NumericValidationBehavior
+	{
+
+		public static readonly BindableProperty ValidationProperty = BindableProperty.CreateAttached("Validation", typeof(bool), typeof(NumericValidationBehavior), false, propertyChanged: OnValidationChanged);
+
+		public static bool GetValidation(BindableObject bindable)
+		{
+			return (bool)bindable.GetValue(ValidationProperty);
+		}
+
+		public static void SetValidation(BindableObject bindable, bool value)
+		{
+			bindable.SetValue(ValidationProperty, value);
+		}
+
+		static void OnValidationChanged(BindableObject bindable, object oldValue, object newValue)
+		{
+			var entry = bindable as Entry;
+			if (entry == null)
+			{
+				return;
+			}
+
+			bool validation = (bool)newValue;
+			if (validation)
+			{
+				entry.TextChanged += OnEntryTextChanged;
+			}
+			else {
+				entry.TextChanged -= OnEntryTextChanged;
+			}
+		}
+
+		static void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+		{
+			double result;
+			bool isValid = double.TryParse(e.NewTextValue, out result);
+			(sender as Entry).TextColor = isValid ? Color.Default : Color.Red;
+		}
+	}
+}
+
