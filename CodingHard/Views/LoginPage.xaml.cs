4a640d686e64810f389d4327f7d2825f7696cafe
山南 <zhangpeichaook@163.com>
2016-06-28 17:40:46 +0800

    Move pages to folder Views

---
+++ CodingHard/Views/LoginPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CodingHard
+{
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage()
+		{
+			InitializeComponent();
+		}
+
+		void SignUp_Clicked(object sender, EventArgs e)
+		{
+			
+		}
+
+		void Login_Clicked(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
+
