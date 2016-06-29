8e6cdb22720f4e2155e1e0cd18112c104ab64d44
山南 <zhangpeichaook@163.com>
2016-06-29 16:41:41 +0800

    BackgroundColorEffect

---
+++ Droid/BackgroundColorEffect.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CodingHard.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+//[assembly: ResolutionGroupName("Shannan")]
+[assembly: ExportEffect(typeof(BackgroundColorEffect), "BackgroundColorEffect")]
+namespace CodingHard.Droid
+{
+	public class BackgroundColorEffect : PlatformEffect
+	{
+		protected override void OnAttached()
+		{
+			try
+			{
+				Control.SetBackgroundColor(Android.Graphics.Color.LightGreen);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Cannot set property on attached control. Error: {0}", ex.Message);
+			}
+		}
+
+		protected override void OnDetached()
+		{
+
+		}
+	}
+}
+
---
+++ iOS/BackgroundColorEffect.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using CodingHard.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+//[assembly: ResolutionGroupName("Shannan")]
+[assembly: ExportEffect(typeof(BackgroundColorEffect), "BackgroundColorEffect")]
+namespace CodingHard.iOS
+{
+	public class BackgroundColorEffect : PlatformEffect
+	{
+
+		protected override void OnAttached()
+		{
+			try
+			{
+				//Control.BackgroundColor = new UIColor.FromRGB(204, 153, 255);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Cannot set property on attached control. Error: {0}", ex.Message);
+			}
+		}
+
+		protected override void OnDetached()
+		{
+
+		}
+	}
+}
+
