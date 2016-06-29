d48e48fb5d81b558945272161eca2be5c1aa4e0c
山南 <zhangpeichaook@163.com>
2016-06-29 16:42:32 +0800

    Model User

---
+++ CodingHard/Models/User.cs
@@ -0,0 +1,24 @@
+﻿namespace CodingHard
+{
+	public class User
+	{
+		public string Username
+		{
+			get;
+			set;
+		}
+
+		public string Password
+		{
+			get;
+			set;
+		}
+
+		public string Email
+		{
+			get;
+			set;
+		}
+	}
+}
+
